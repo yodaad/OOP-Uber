1a1db78473b52f9597ec03149f2e2b9f2b4da805
yodaad@gmail.com <yodaad@gmail.com>
2023-05-11 20:38:45 -0500

    index.php file updated

--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,14 @@
 // Index file
 
 <?php
+require_once("car.php");
+require_once("uberX.php");
+require_once("uberPool.php");
+require_once("account.php");
 
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
+
+$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
+$uberPool->printDataCar();
 ?>
